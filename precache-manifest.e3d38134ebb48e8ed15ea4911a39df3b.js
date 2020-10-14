e774164cc3f1dfb20398474b82a6d3c4a09d1bcc
Kirby, Matt J (UG - Computer Science) <mk01311@surrey.ac.uk>
2020-10-14 15:51:22 +0100

    Updates

---
+++ precache-manifest.e3d38134ebb48e8ed15ea4911a39df3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b629220751578c64783d48a5eea385a",
+    "url": "/Simple-React-UI/index.html"
+  },
+  {
+    "revision": "c112def76bf89b749cef",
+    "url": "/Simple-React-UI/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f148b928f15232515557",
+    "url": "/Simple-React-UI/static/css/main.26896ff3.chunk.css"
+  },
+  {
+    "revision": "c112def76bf89b749cef",
+    "url": "/Simple-React-UI/static/js/2.fee2cb02.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/Simple-React-UI/static/js/2.fee2cb02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f148b928f15232515557",
+    "url": "/Simple-React-UI/static/js/main.b15c4aed.chunk.js"
+  },
+  {
+    "revision": "3e6dbd5c3c4159f48380",
+    "url": "/Simple-React-UI/static/js/runtime-main.6d17f39e.js"
+  }
+]);
